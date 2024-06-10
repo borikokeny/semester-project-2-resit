37ca8742baa6dfc2e503697ef9026bbff3bf5c61
Bori Kokeny <bori@minerali.hu>
2024-06-10 14:22:45 +0200

    Adds a better listing page

--- src/js/templates/listing.mjs
+++ src/js/templates/listing.mjs
@@ -22,8 +22,40 @@ export async function renderSingleListingPage() {
   img.src = listing.media[0] ?? `/images/img-placeholder.png`;
   title.innerHTML = listing.title;
   description.innerHTML = listing.description;
-  endsAt.innerHTML = listing.endsAt;
-  endsAt.textContent = `Listing ends at ${endsAt.innerText}`;
+
+  if(listing.endsAt) {
+    const endsAtDate = document.querySelector("#listingEndsAt");
+
+    // const endsAtDate = document.createElement("p");
+
+    const today = new Date();
+    const timeLeft = new Date(listing.endsAt);
+
+    const timeCounter = timeLeft - today;
+    const secondsToDate = Math.floor((timeCounter % (1000 * 60)) / (1000));
+    const minutesToDate = Math.floor((timeCounter % (1000 * 60 * 60)) / (1000 * 60));
+    const hoursToDate = Math.floor((timeCounter % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const daysCount = Math.ceil(timeCounter / (1000 * 60 * 60 * 24));
+    console.log(daysCount);
+
+    endsAtDate.classList.add("mt-5");
+
+    endsAtDate.innerText = `Closes in ${daysCount} days ${hoursToDate} hours ${minutesToDate} minutes ${secondsToDate} seconds`;
+
+    // if (timeCounter <= 0) {
+    //   clearInterval(counterInterval);
+    //   endsAtDate.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> Listing Ended`;
+    // }
+
+    description.append(endsAtDate);
+
+
+
+  }
+
+  // endsAt.innerHTML = listing.endsAt;
+  // endsAt.textContent = `Listing ends at ${endsAt.innerText}`;
+
   bids.innerHTML = listing._count.bids;
   bids.textContent = `Current BID: ${bids.innerText}`;
   sellerAvatar.src = listing.avatar ?? `/images/img-placeholder.png`;
